d9c878c83932c59a6317207df51fb3933ecfbfc2
Vilen23 <shivamlather5@gmail.com>
2024-07-10 02:39:22 +0530

    slices

--- slice_type.rs
+++ slice_type.rs
@@ -1,18 +1,17 @@
-fn main(){
-    let s = String::from("String is new");
-
-    let ans:usize = first_word(&s);
+fn main() {
+    let mut s = String::from("String is new");
+    let ans: &str= first_word(&s)
+    s.clear(); //mutable borrow cannot occur as immutable borrow is used after this
     println!("{ans}");
 }
 
-fn first_word(s:&String)->usize {
+fn first_word(s: &str) ->&str { 
     let array = s.as_bytes();
 
-    for(i,&item) in array.iter().enumerate() {
-        if item == b' '{
-            return i;
+    for (i, &item) in array.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
         }
     }
-
-    s.len()
+    &s[..]
 }
